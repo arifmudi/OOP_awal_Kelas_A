38ef50ad22b03a8a595fd2eb5bb3de3c99fb992d
abdurrahman12001 <149956912+abdurrahman12001@users.noreply.github.com>
2023-11-30 12:43:49 +0700

    abdurrahman.php

--- abdurrahman.php
+++ abdurrahman.php
@@ -8,38 +8,36 @@ class mahasiswa {
   private $name;
   private $nim;
   private $matkul;
-
+  private $cita_cita;
   // Methods
-  function set_name($name) {
-    $this->name = $name;
+  function __construct($name, $nim, $matkul, $cita_cita){
+      $this->name = $name;
+      $this->nim = $nim;
+      $this->matkul = $matkul;
+      $this->cita_cita = $cita_cita;
   }
   function get_name() {
     return $this->name;
   }
-  function set_nim($nim) {
-    $this->nim = $nim;
-  }
   function get_nim() {
     return $this->nim;
-  }
-  function set_matkul($matkul) {
-    
-  	$this->matkul = $matkul;
   }
   function get_matkul () {
   	return $this->matkul;
   }
+  function get_cita_cita () {
+  	return $this->cita_cita;
+  }
 }
 
-$roman = new mahasiswa();
-$roman->set_name('Abdurrahman');
-$roman->set_nim('2255201009');
-$roman->set_matkul('pbo');
+$roman = new mahasiswa('Abdurrahman', '2255201009', 'PBO', 'Pengusaha');
 echo "nama: " . $roman->get_name();
 echo "<br>";
 echo "nim: " .  $roman->get_nim();
 echo "<br>";
 echo "matkul: " . $roman->get_matkul();
+echo "<br>";
+echo "matkul: " . $roman->get_cita_cita();
 ?>
  
 </body>
